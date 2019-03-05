c34f80040bd398748be64eca0dbb1cddfc90eb35
Katharine Cooney <katharinecooney@gmail.com>
2019-03-05 14:00:22 +0900

    Add moveBackward() function; update condition for moveForward section of obeyCommand() function

--- javascripts/app.js
+++ javascripts/app.js
@@ -44,20 +44,36 @@ function turnRight(roverName){
 }
 
 function moveForward(roverName){
-  if (rover.direction === "N") {
-    rover.y++;
-  } else if (rover.direction === "S") {
+    if (rover.direction === "N" && rover.y <= 9) {
+      rover.y++;
+    } else if (rover.direction === "S" && rover.y >= 1) {
+      rover.y--;
+    } else if (rover.direction === "E" && rover.x <= 9) {
+      rover.x++;
+    } else if (rover.direction === "W" && rover.x >= 1) {
+      rover.x--;
+    } else {
+      console.log("You're going off the grid!");
+    }
+}
+
+function moveBackward(roverName){
+  if (rover.direction === "N" && rover.y >= 1) {
     rover.y--;
-  } else if (rover.direction === "E") {
-    rover.x++;
-  } else if (rover.direction === "W") {
+  } else if (rover.direction === "S" && rover.y <= 9) {
+    rover.y++;
+  } else if (rover.direction === "E" && rover.x >= 1) {
     rover.x--;
+  } else if (rover.direction === "W" && rover.x <= 9) {
+    rover.x++;
+  } else {
+    console.log("You're going off the grid!");
   }
 }
 
 
 function obeyCommand(){
-  var userCommand = prompt("Enter your commands! Turn right: r, Turn left: l, Move forward: f, Quit: quit"); 
+  var userCommand = prompt("Enter your commands! Turn right: r, Turn left: l, Move forward: f, Move backward: b, Quit: quit"); 
   while (userCommand !== 'quit') {
     for (var i = 0; i < userCommand.length; i++){
       if (userCommand[i] === 'r') {
@@ -65,13 +81,14 @@ function obeyCommand(){
       } else if (userCommand[i] === 'l') {
         turnLeft();
       } else if (userCommand[i] === 'f') {
-
         moveForward();
-      } 
-      console.log("The rover is located at " + rover.x + ", " + rover.y);
+      } else if (userCommand[i] === 'b') {
+        moveBackward();
+      }
       rover.travelLog.push(rover.x + ", " + rover.y);
     }
-    var userCommand = prompt("what is the command?"); 
+    console.log("The rover is located at " + rover.x + ", " + rover.y);
+    var userCommand = prompt("Enter your commands! Turn right: r, Turn left: l, Move forward: f, Move backward: b, Quit: quit"); 
   }
 }
 
